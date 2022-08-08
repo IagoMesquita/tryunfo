fca161a77526478654182989cf0e0ce6d67ca81e
Iago Mesquita <iagomesquitaalves@gmail.com>
2022-08-08 18:33:01 -0300

    refactor: start refactoring app from class to function

--- src/App.js
+++ src/App.js
@@ -1,181 +1,176 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
-import PropTypes from 'prop-types';
 import Form from './components/Form';
 import Card from './components/Card';
 import ListOfCards from './components/ListOfCards';
 
-const INITIAL_STATE = {
+const INITIAL_CARD = {
   cardName: '',
   cardDescription: '',
-  cardAttr1: '0',
-  cardAttr2: '0',
-  cardAttr3: '0',
+  cardAttr1: '',
+  cardAttr2: '',
+  cardAttr3: '',
   cardImage: '',
   cardRare: 'normal',
   cardTrunfo: false,
-  // hasTrunfo: false,
   isSaveButtonDisabled: true,
-  // onSaveButtonClick: false,
 };
-class App extends React.Component {
-  constructor() {
-    super();
 
-    this.onInputChange = this.onInputChange.bind(this);
-    this.isEmpty = this.isEmpty.bind(this);
-    this.onSaveButtonClick = this.onSaveButtonClick.bind(this);
-    this.resetState = this.resetState.bind(this);
-    this.checkedHasTrunfo = this.checkedHasTrunfo.bind(this);
+function App() {
+  const [card, setCard] = useState({
+    cardName: '',
+    cardDescription: '',
+    cardAttr1: '',
+    cardAttr2: '',
+    cardAttr3: '',
+    cardImage: '',
+    cardRare: '',
+    cardTrunfo: false,
+  });
 
-    this.state = {
-      cardName: '',
-      cardDescription: '',
-      cardAttr1: '',
-      cardAttr2: '',
-      cardAttr3: '',
-      cardImage: '',
-      cardRare: '',
-      cardTrunfo: false,
-      hasTrunfo: false,
-      isSaveButtonDisabled: true,
-      // onSaveButtonClick: false,
-      storageCard: [],
-    };
-  }
+  const [isSaveButtonDisabled, setIsSaveButtonDisabled] = useState(true);
+  // const [hasTrunfo, setHasTrunfo] = useState(false);
+  const [storageCard, setStorageCard] = useState([]);
 
-  // checkedTrunfo () {
-  //   const { storageCard } = this.state;
-  //   storageCard.checked === true && this.setState({})
-  // }
+  // const checkedHasTrunfo = () => {
+  //   const { cardTrunfo } = card;
+  //   return cardTrunfo && this.setState({ hasTrunfo: true });
+  // };
 
-  onSaveButtonClick(event) {
+  const resetState = () => {
+    setCard({ ...INITIAL_CARD });
+    setIsSaveButtonDisabled(true);
+    // this.setState({ ...INITIAL_STATE });
+  };
+
+  const onSaveButtonClick = (event) => {
     event.preventDefault();
-    this.checkedHasTrunfo();
-    this.setState(({
-      storageCard, cardName, cardDescription, cardImage,
-      cardRare, cardAttr1, cardAttr2, cardAttr3,
-    }) => ({
-      storageCard: [...storageCard,
-        { cardName,
-          cardDescription,
-          cardImage,
-          cardRare,
-          cardAttr1,
-          cardAttr2,
-          cardAttr3 }],
-    }));
-    this.resetState();
-  }
+    checkedHasTrunfo();
+    setStorageCard([...storageCard, card]);
+    // this.setState(
+    //   ({
+    //     storageCard,
+    //     cardName,
+    //     cardDescription,
+    //     cardImage,
+    //     cardRare,
+    //     cardAttr1,
+    //     cardAttr2,
+    //     cardAttr3,
+    //   }) => ({
+    //     storageCard: [
+    //       ...storageCard,
+    //       {
+    //         cardName,
+    //         cardDescription,
+    //         cardImage,
+    //         cardRare,
+    //         cardAttr1,
+    //         cardAttr2,
+    //         cardAttr3,
+    //       },
+    //     ],
+    //   }),
+    // );
+    resetState();
+  };
 
-  onInputChange({ target }) {
+  const onInputChange = ({ target }) => {
     const { name } = target;
     const value = target.type === 'checkbox' ? target.checked : target.value;
-    this.setState({
-      [name]: value,
-    }, this.isEmpty);
-  }
+    setCard({ ...card, [name]: value });
+    // this.setState(
+    //   {
+    //     [name]: value,
+    //   },
+    //   this.isEmpty,
+    // );
+  };
 
-  checkedHasTrunfo() {
-    const { cardTrunfo } = this.state;
-    return cardTrunfo && this.setState({ hasTrunfo: true });
-  }
+  // VERIFICA TOTAL DA PONTUACAO
+  // const isEmpty = () => {
+  //   const {
+  //     cardName,
+  //     cardDescription,
+  //     cardImage,
+  //     cardRare,
+  //     cardAttr1,
+  //     cardAttr2,
+  //     cardAttr3,
+  //   } = this.state;
 
-  resetState() {
-    this.setState({ ...INITIAL_STATE });
-  }
+  //   const isActiveString = [cardName, cardDescription, cardImage, cardRare];
+  //   const isActiveNumbers = [
+  //     Number(cardAttr1),
+  //     Number(cardAttr2),
+  //     Number(cardAttr3),
+  //   ];
 
-  isEmpty() {
-    const { cardName, cardDescription, cardImage, cardRare,
-      cardAttr1, cardAttr2, cardAttr3,
-    } = this.state;
+  //   const maxAttrValue = 90;
+  //   const maxSumOfValues = 210;
+  //   const sumAttr = Number(cardAttr1) + Number(cardAttr2) + Number(cardAttr3);
 
-    const isActiveString = [
-      cardName,
-      cardDescription,
-      cardImage,
-      cardRare,
-    ];
-    const isActiveNumbers = [
-      Number(cardAttr1),
-      Number(cardAttr2),
-      Number(cardAttr3),
-    ];
+  //   const hasInputString = isActiveString.every((string) => string.length > 0);
+  //   const hasInputNumbers = isActiveNumbers.every(
+  //     (number) => number >= 0 && number <= maxAttrValue,
+  //   );
+  //   const ValueSumAreCorrect = sumAttr <= maxSumOfValues;
 
-    const maxAttrValue = 90;
-    const maxSumOfValues = 210;
-    const sumAttr = Number(cardAttr1) + Number(cardAttr2) + Number(cardAttr3);
+  //   const hasInputValue = hasInputString && hasInputNumbers && ValueSumAreCorrect;
+  //   // console.log(`hasInputValue ${hasInputValue}`);
 
-    const hasInputString = isActiveString.every((string) => string.length > 0);
-    const hasInputNumbers = isActiveNumbers.every((number) => number >= 0
-    && number <= maxAttrValue);
-    const ValueSumAreCorrect = sumAttr <= maxSumOfValues;
+  //   this.setState({
+  //     isSaveButtonDisabled: !hasInputValue,
+  //   });
+  // };
 
-    const hasInputValue = (hasInputString && hasInputNumbers && ValueSumAreCorrect);
-    // console.log(`hasInputValue ${hasInputValue}`);
-
-    this.setState({
-      isSaveButtonDisabled: !hasInputValue,
-    });
-  }
-
-  render() {
-    const {
-      cardName, cardDescription, cardAttr1, cardAttr2, cardAttr3, cardImage,
-      cardRare, cardTrunfo, isSaveButtonDisabled, hasTrunfo, checkedHasTrunfo,
-      storageCard,
-    } = this.state;
-
-    return (
-      <div className="app">
-        <div className="creatCard">
-          <Form
-            { ...this.state }
-            // cardName={ cardName }
-            // cardDescription={ cardDescription }
-            // cardAttr1={ cardAttr1 }
-            // cardAttr2={ cardAttr2 }
-            // cardAttr3={ cardAttr3 }
-            // cardImage={ cardImage }
-            // cardRare={ cardRare }
-            // cardTrunfo={ cardTrunfo }
-            hasTrunfo={ hasTrunfo }
-            isSaveButtonDisabled={ isSaveButtonDisabled }
-            onSaveButtonClick={ this.onSaveButtonClick }
-            onInputChange={ this.onInputChange }
-            isEmpty={ this.isEmpty }
-            checkedHasTrunfo={ checkedHasTrunfo }
-          />
-          <Card
-            cardName={ cardName }
-            cardDescription={ cardDescription }
-            cardAttr1={ cardAttr1 }
-            cardAttr2={ cardAttr2 }
-            cardAttr3={ cardAttr3 }
-            cardImage={ cardImage }
-            cardRare={ cardRare }
-            cardTrunfo={ cardTrunfo }
-            storageCard={ storageCard }
-          />
-        </div>
-        <div className="listCart">
-          <ListOfCards
-            storageCard={ storageCard }
-          />
-        </div>
+  return (
+    <div className="app">
+      <div className="creatCard">
+        <Form
+          card={ card }
+          // cardName={ cardName }
+          // cardDescription={ cardDescription }
+          // cardAttr1={ cardAttr1 }
+          // cardAttr2={ cardAttr2 }
+          // cardAttr3={ cardAttr3 }
+          // cardImage={ cardImage }
+          // cardRare={ cardRare }
+          // cardTrunfo={ cardTrunfo }
+          // hasTrunfo={ hasTrunfo }
+          isSaveButtonDisabled={ isSaveButtonDisabled }
+          onSaveButtonClick={ onSaveButtonClick }
+          onInputChange={ onInputChange }
+          // isEmpty={ isEmpty }
+          // checkedHasTrunfo={ checkedHasTrunfo }
+        />
+        <Card
+          cardName={ card.cardName }
+          cardDescription={ card.cardDescription }
+          cardAttr1={ card.cardAttr1 }
+          cardAttr2={ card.cardAttr2 }
+          cardAttr3={ card.cardAttr3 }
+          cardImage={ card.cardImage }
+          cardRare={ card.cardRare }
+          cardTrunfo={ card.cardTrunfo }
+          storageCard={ storageCard }
+        />
       </div>
-    );
-  }
+      <div className="listCart">
+        <ListOfCards storageCard={ storageCard } />
+      </div>
+    </div>
+  );
 }
-App.propTypes = {
-  onInputChange: PropTypes.func.isRequired,
-  isEmpty: PropTypes.func.isRequired,
-  maxAttrValue: PropTypes.number.isRequired,
-  maxSumOfValues: PropTypes.number.isRequired,
-  sumAttr: PropTypes.number.isRequired,
-  hasInputString: PropTypes.bool.isRequired,
-  hasInputNumbers: PropTypes.bool.isRequired,
-  ValueSumAreCorrect: PropTypes.bool.isRequired,
-  hasInputValue: PropTypes.bool.isRequired,
-};
+// App.propTypes = {
+//   onInputChange: PropTypes.func.isRequired,
+//   isEmpty: PropTypes.func.isRequired,
+//   maxAttrValue: PropTypes.number.isRequired,
+//   maxSumOfValues: PropTypes.number.isRequired,
+//   sumAttr: PropTypes.number.isRequired,
+//   hasInputString: PropTypes.bool.isRequired,
+//   hasInputNumbers: PropTypes.bool.isRequired,
+//   ValueSumAreCorrect: PropTypes.bool.isRequired,
+//   hasInputValue: PropTypes.bool.isRequired,
+// };
 export default App;
